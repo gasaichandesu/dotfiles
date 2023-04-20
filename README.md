4f997f7961451b1a507c7ec7f6036a405ad48b6c
gasaichandesu <bjadmund@mail.ru>
2023-04-20 11:21:33 +0400

    Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dotfiles
+
+This repository is designed to inject required dotfiles using stow.
+
+Example usage:
+```bash
+stow vim
+```
+
+As a result, a symlink in home folder will be created with same structure as in vim folder
--- vim/.config/nvim/lua/lsp.lua
+++ vim/.config/nvim/lua/lsp.lua
@@ -122,6 +122,6 @@ lsp_installer.on_server_ready(
 require "flutter-tools".setup {
   lsp = {
     on_attach = on_attach,
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
   }
 }
